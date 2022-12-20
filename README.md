2b8fea03f91f9ab939ed63f86c82c6c1154ba3f1
unknown <andrey.detochkin@gmail.com>
2022-12-20 15:11:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+
+
+
+![PIC](https://blogs.mulesoft.com/wp-content/uploads/api-connect-devices.png)
+
+
+## RECORD STORE DB 
+
+```
+Description
+
+This is a project with possibility
+to CRUD data of the small record store 
+using MySQL.
+
+Frontend is created with React
+
+
+
+```
+
+#### Andrejs Grocs 2022
---
+++ backend/index.js
@@ -0,0 +1,65 @@
+
+import express from "express";
+import mysql from "mysql";
+
+
+const app = express();
+
+
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"password",
+    database:"records_store"
+})
+
+app.use(express.json())
+
+
+
+
+// Getting the clg message ib the screen to test that DB is working
+
+app.get("/", (req, res) => {
+    res.json("Hello from server");
+  });
+
+
+// Getting data form DB on the screen using the SQL command
+
+app.get("/records", (req, res)=>{
+    const q = 'SELECT * FROM records_store.records'
+    db.query(q,(err, data)=>{
+        if(err)
+        return res.json(err)
+        return res.json(data)
+    })
+
+
+ })
+
+app.post("/records", (req,res)=>{
+    const q = "INSERT INTO records(`artist`,`title`,`label`,`cover`,`genre`,`year`) VALUES (?)";
+    const values = [
+        //"title","title","title","title","title","title",
+
+
+        req.body.artist,
+        req.body.title,
+        req.body.label,
+        req.body.cover,
+        req.body.genre,
+        req.body.year,
+]
+
+    db.query(q, [values], (err, data)=>{
+        if(err)
+        return res.send(err);
+        return res.json('Records has been created');
+    })
+})
+
+app.listen(8800, ()=>{
+    console.log('Connected to the backend')
+})
